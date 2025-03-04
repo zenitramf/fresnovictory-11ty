b695d094c22e124ac8a4a69c528542502efda407
Francisco Martinez <francisco@zenitram.dev>
2025-03-03 23:18:29 -0800

    inital commit

---
+++ eleventy.config.js
@@ -0,0 +1,30 @@
+import pluginWebc from "@11ty/eleventy-plugin-webc";
+
+export default function (eleventyConfig) {
+  eleventyConfig.setServerOptions({
+    watch: ["./_src/sass/styles.scss"],
+    domDiff: false
+  })
+  eleventyConfig.addPlugin(pluginWebc, {
+    components: "./_includes/webc/*.webc"
+  });
+  eleventyConfig.addJavaScriptFunction("serviceTimes", function () {
+    const serviceTimes = [
+      { time: "10:00am", desc: "Sunday School" },
+      { time: "11:00am", desc: "Sunday Morning" },
+      { time: "5:30pm", desc: "Sunday Evening" },
+      { time: "7:00pm", desc: "Thursday Evening" }
+    ];
+    return serviceTimes
+  });
+
+  eleventyConfig.addJavaScriptFunction("currentDate", function () {
+    const today = new Date()
+    return today.getFullYear()
+  })
+
+}
+
+export const config = {
+}
+
